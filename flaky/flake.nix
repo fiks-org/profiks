670bd4d273318176b864d0be5c3efdc1868c957f
Andrew Kvapil <git@viluon.me>
2023-11-25 01:51:55 +0100

    Initial commit

---
+++ flaky/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  let pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python3
+      ];
+    };
+  };
+}
